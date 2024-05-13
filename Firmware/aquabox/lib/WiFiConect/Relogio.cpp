5916b28f105d311f280cb924c054a552ae469995
José Adriano <adriano.liukin@gmail.com>
2024-05-13 09:25:25 -0300

    Atualização

---
+++ Firmware/aquabox/lib/WiFiConect/Relogio.cpp
@@ -0,0 +1,60 @@
+#include "Relogio.hpp"
+
+void wifiInit(void)
+{
+
+}
+
+void taskRelogio( void *params)
+{
+    const char* ntpServer = "pool.ntp.org";
+    const long  gmtOffset_sec = -14400; //GMT Time Brazil
+    const int   daylightOffset_sec = 3600;
+
+    // Init and get the time
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    printLocalTime();
+
+    while(true)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        printLocalTime();
+    }
+}
+
+void printLocalTime(void)
+{
+  struct tm timeinfo;
+
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+
+}
---
+++ Firmware/aquabox/lib/WiFiConect/Relogio.hpp
@@ -0,0 +1,11 @@
+#ifndef RELOGIO_H
+#define RELOGIO_H
+
+#include "Arduino.h"
+#include <time.h>
+
+//protótipo das funções
+void printLocalTime(void);
+void taskRelogio( void *params);
+
+#endif //RELOGIO_H
