ace4e23b88472d612268c5177798e20f17c2a0c0
José Adriano <adriano.liukin@gmail.com>
2024-05-14 09:21:59 -0300

    Atualização

--- Firmware/aquabox/src/main.cpp
+++ Firmware/aquabox/src/main.cpp
@@ -1,11 +1,16 @@
 #include "Arduino.h"
 #include <WiFi.h>
 #include "WiFiManager.h"
-#include "Sensores.hpp"
+#include "EventoSensores.hpp"
 #include "Relogio.hpp"
 #include "Reles.hpp"
 
-//Protótipo das funções
+//Protótipo das funções e tasks
+void taskSensores (void *params);
+void nivelBaixoPressionado();
+void nivelAltoPressionado();
+void nivelBaixoLiberado();
+void nivelAltoLiberado();
 
 void setup() 
 {
@@ -42,4 +47,57 @@ void loop()
 {
   
 }
+
+/* --------------------------------------------------*/
+/* ---------------------- Tarefas -------------------*/
+/* --------------------------------------------------*/
+
+void taskSensores (void *params)
+{
+
+  #define SENSOR_NIVEL_BAIXO 27
+  #define SENSOR_NIVEL_ALTO 14
+  //const int SENSOR_NIVEL_BAIXO = 27;
+  //const int SENSOR_NIVEL_ALTO = 14;
+
+  EventoSensores nivelBaixo(SENSOR_NIVEL_BAIXO, LOW);
+  EventoSensores nivelAlto(SENSOR_NIVEL_ALTO, LOW);
+
+  nivelBaixo.setPressionadoCallback(&nivelBaixoPressionado);
+  nivelAlto.setPressionadoCallback(&nivelAltoPressionado);
+  nivelBaixo.setLiberadoCallback(&nivelBaixoLiberado);
+  nivelAlto.setLiberadoCallback(&nivelAltoLiberado);
+
+  while(true)
+  {
+      nivelBaixo.process();
+      nivelAlto.process();
+
+      /* Espera um segundo */
+      vTaskDelay( 100 / portTICK_PERIOD_MS ); 
+
+
+  }
+}
+
+void nivelBaixoPressionado()
+{
+    Serial.println("Pressionou o sensor de nivel BAIXO");
+}
+
+void nivelAltoPressionado()
+{
+    Serial.println("Pressionou o sensor de nivel ALTO");
+}
+
+void nivelBaixoLiberado()
+{
+    Serial.println("Liberado o sensor de nivel BAIXO");
+}
+
+void nivelAltoLiberado()
+{
+    Serial.println("Liberado o sensor de nivel ALTO");
+}
+
 
